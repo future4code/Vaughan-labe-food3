0856680f6a8f1e5fb935afd7525e6c2a0f93ef45
Rodrigo Brito <rodrigo.lab@yahoo.com>
2022-03-15 14:33:51 -0300

    Criação da logica da tela de cadastro de usuário

--- src/Componentes/Cadastro/Cadastro.js
+++ src/Componentes/Cadastro/Cadastro.js
@@ -1,19 +1,128 @@
 import React from "react";
+import axios from "axios";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const Cadastro= () => {
-    const navigate=useNavigate();
+const Cadastro = () => {
+  const [nome, setNome] = useState();
+  const [email, setEmail] = useState();
+  const [cpf, setCpf] = useState();
+  const [senha, setSenha] = useState();
+  const [conf_senha, setConf_senha] = useState();
 
-    const cadastrarEndereco= () => {
-        navigate("/cadastro/endereco");
-    }
-    return (
-        <div>
-            <h1>Cadastrar</h1>
-            <button onClick={cadastrarEndereco}>Criar</button>
-        </div>
-    )
+  const navigate = useNavigate();
+
+  const cadastrarEndereco = () => {
+    navigate("/cadastro/endereco");
+  };
+
+  const manNome = (event) => {
+    setNome(event.target.value);
+  };
+
+  const manEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const manCpf = (event) => {
+    setCpf(event.target.value);
+  };
+
+  const manSenha = (event) => {
+    setSenha(event.target.value);
+  };
+
+  const manConfirmaSenha = (event) => {
+    setConf_senha(event.target.value);
+  };
+
+  const limpaInput = () => {
+    setNome("");
+    setEmail("");
+    setSenha("");
+    setCpf("");
+    setConf_senha("");
+  };
+
+  const fazCadstro = (event) => {
+    event.preventDefault();
+
+    const body = {
+      name: nome,
+      email: email,
+      cpf: cpf,
+      password: senha,
+    };
+
+    axios
+      .post(
+        "https://us-central1-missao-newton.cloudfunctions.net/fourFoodB/signup",
+        body
+      )
+
+      .then((resp) => {
+        alert("Otimo, o cadastro do perfil foi efetuado com Sucesso!");
+        limpaInput();
+        cadastrarEndereco();
+      })
+      .catch((err) => {
+        alert(
+          "Ops, algo deu errado, tente novamente em alguns instantes:",
+          err.response
+        );
+      });
+  };
+
+  return (
+    <div>
+      <h1>Cadastrar</h1>
+
+      <form onSubmit={fazCadstro}>
+        <input
+          placeholder="Nome e sobrenome"
+          type="text"
+          value={nome}
+          onChange={manNome}
+          required
+        />
+        <input
+          placeholder="E-mail"
+          type="text"
+          value={email}
+          onChange={manEmail}
+          required
+        />
+        <input
+          placeholder="CPF"
+          value={cpf}
+          onChange={manCpf}
+          type="text"
+          name="cpf"
+          pattern="(\d{3}\.?\d{3}\.?\d{3}-?\d{2})|(\d{2}\.?\d{3}\.?\d{3}/?\d{4}-?\d{2})"
+          title={"Digite um CPF no formato: xxx.xxx.xxx-xx"}
+          required
+        />
+        <input
+          placeholder="Senha"
+          type="password"
+          value={senha}
+          onChange={manSenha}
+          required
+        />
+        <input
+          placeholder="Confirme a senha anterior"
+          type="password"
+          pattern=".{6,}"
+          title={"No mínimo 6 numeros"}
+          value={conf_senha}
+          onChange={manConfirmaSenha}
+          required
+        />
+
+        <button> Criar </button>
+      </form>
+    </div>
+  );
 };
-
 
 export default Cadastro;
