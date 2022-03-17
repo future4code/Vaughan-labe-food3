65fceb2ae83d1196b417b080c2245ea93a9d5b9c
Rodrigo Brito <rodrigo.lab@yahoo.com>
2022-03-17 15:52:18 -0300

    Estilização do Login e ajustes

--- src/Componentes/Login/Login.js
+++ src/Componentes/Login/Login.js
@@ -2,75 +2,77 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import useForms from "../Hook/useForms";
-import {useSemProtecao} from "../useSemProtecao/useSemProtecao";
+import { useSemProtecao } from "../useSemProtecao/useSemProtecao";
+import { Base, Logo, Titulo, Input, Botao, BotaoVazio, Bloco } from "./styled";
+import Logo1 from "../Media/logo1.png";
 
-const Login= () => {
-    const navigate= useNavigate();
-    const {form, onChangeForm, clear}= useForms({email:"", password:""})
-    useSemProtecao();
+const Login = () => {
+  const navigate = useNavigate();
+  const { form, onChangeForm, clear } = useForms({ email: "", password: "" });
+  useSemProtecao();
 
-    const onSubmitForm= event => {
-        event.preventDefault();
-        const url=`https://us-central1-missao-newton.cloudfunctions.net/fourFoodB/login`;
+  const onSubmitForm = (event) => {
+    event.preventDefault();
+    const url = `https://us-central1-missao-newton.cloudfunctions.net/fourFoodB/login`;
 
-        const header={
-            headers:{
-                "Content-Type": "application/json"
-            }
-        };
-
-        axios
-            .post(url, form, header)
-            .then(response => {
-                if(response.data.user.hasAddress === false){
-                    return navigate("/cadastro/endereco");
-                };
-                
-                localStorage.setItem("token", response.data.token)
-                navigate("/home");
-            })
-            .catch(() => {
-                alert("Usuário não cadastrado")
-                clear();
-            })
+    const header = {
+      headers: {
+        "Content-Type": "application/json",
+      },
     };
 
-    const irParaCadastro= () => {
-        navigate("/cadastro");
-    };
-    
-    return (
-        <div>
-            <h1>Entrar</h1>
+    axios
+      .post(url, form, header)
+      .then((response) => {
+        if (response.data.user.hasAddress === false) {
+          return navigate("/cadastro/endereco");
+        }
 
-            <form onSubmit={onSubmitForm}>
-                <input
-                    type="email"
-                    placeholder="email@email.com"
-                    name="email"
-                    value={form.email}
-                    onChange={onChangeForm}
-                    required
-                />
+        localStorage.setItem("token", response.data.token);
+        navigate("/home");
+      })
+      .catch(() => {
+        alert("Usuário não cadastrado");
+        clear();
+      });
+  };
 
-                <input
-                    type="password"
-                    placeholder="Senha"
-                    name="password"
-                    value={form.password}
-                    onChange={onChangeForm}
-                    pattern="^.{6,}"
-                    title="Sua senha deve ter no mínimo 6 caracteres"
-                    required
-                />
+  const irParaCadastro = () => {
+    navigate("/cadastro");
+  };
 
-                <button>Entrar</button>
-            </form>
+  return (
+    <Base>
+      <Bloco> <Logo src={Logo1} /> </Bloco>
+      <Bloco> <Titulo> Entrar </Titulo> </Bloco>
 
-            <button onClick={irParaCadastro}>Não possui cadastro? Clique aqui</button>
-        </div>
-    )
+      <form onSubmit={onSubmitForm}>
+        <Input
+          type="email"
+          placeholder="email@email.com"
+          name="email"
+          value={form.email}
+          onChange={onChangeForm}
+          required
+        />
+
+        <Input
+          type="password"
+          placeholder="Senha"
+          name="password"
+          value={form.password}
+          onChange={onChangeForm}
+          pattern="^.{6,}"
+          title="Sua senha deve ter no mínimo 6 caracteres"
+          required
+        />
+
+        <Botao> Entrar </Botao>
+      </form>
+
+      <BotaoVazio onClick={irParaCadastro}>Não possui cadastro? Clique aqui</BotaoVazio>
+    </Base>
+  );
 };
-
 
 export default Login;
